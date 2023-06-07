9dc14e151439378f9fc85c4f56dab6ab9848eeec
LiuAoran <aoran_liu@outlook.com>
2023-06-08 01:48:22 +0800

    Init LoginWindow

--- Wolah.IM/Wolah.IM/Private/ServerSource.cs
+++ Wolah.IM/Wolah.IM/Private/ServerSource.cs
@@ -4,8 +4,8 @@ namespace Wolah.IM.Private;
 public static class ServerSource
 {
 #if true
-    public static IPAddress ServerIP { get; } = IPAddress.Parse("xxx.xxx.xxx.xxx");
-    public static int ServerPort { get; } = 8888;
+    public static IPAddress ServerIP { get; } = IPAddress.Parse("***REMOVED***");
+    public static int ServerPort { get; } = 2339;
 #else
     public static IPAddress ServerIP { get; } = IPAddress.Parse("127.0.0.1");
     public static int ServerPort { get; } = 8888;
