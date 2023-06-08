a86d5612cf2ede0ca9ca4f6ff093e48384408254
LiuAoran <aoran_liu@outlook.com>
2023-06-08 09:03:46 +0800

    gititgore

--- Wolah.IM/Wolah.IM/Private/ServerSource.cs
+++ Wolah.IM/Wolah.IM/Private/ServerSource.cs
@@ -4,7 +4,7 @@ namespace Wolah.IM.Private;
 public static class ServerSource
 {
 #if true
-    public static IPAddress ServerIP { get; } = IPAddress.Parse("***REMOVED***");
+    public static IPAddress ServerIP { get; } = IPAddress.Parse("121.37.111.190");
     public static int ServerPort { get; } = 2339;
 #else
     public static IPAddress ServerIP { get; } = IPAddress.Parse("127.0.0.1");
