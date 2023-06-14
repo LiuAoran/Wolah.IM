408a4301c2c835b5eca990edb8994ae5aa9b85d1
flyjay123 <118870803+flyjay123@users.noreply.github.com>
2023-06-14 09:09:45 +0800

    Register Control

---
+++ Wolah.IM/Wolah.IM/View/LoginView/RegisterControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Wolah.IM.View.LoginView;
+
+public partial class RegisterControl : UserControl
+{
+    public event EventHandler GoLoginEvent;
+    
+    public RegisterControl()
+    {
+        InitializeComponent();
+    }
+
+    private void GoLoginBtn_OnClick(object sender, RoutedEventArgs e)
+    {
+        GoLoginEvent?.Invoke(this, EventArgs.Empty);
+    }
+    
+}
